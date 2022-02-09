d7f731f7fc515fa44c5bf63e503e504c52659ef7
David Stinson <49245298+DavidStinson@users.noreply.github.com>
2022-02-09 10:36:00 -0600

    add html

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
+// TODO: Add the HTML for two buttons (one for T-Swift, one for Yeezy)
+// TODO: Add a container element for the card components to be appended to
+// TODO: Add cached element references for each of the buttons
+// TODO: Add a cached element reference for the container element in the JS file
+// TODO: Add event listeners to each of the buttons
+// TODO: Write and export functions to access our quote data
+// TODO: Import the functions that will access the quote data
+// TODO: Ensure the functions that we have built will work as intended.
+// TODO: Tweak event listeners so that the quote is stored in a variable
+// TODO: Create a test card element with Bootstrap
+// TODO: Create a render function
+// TODO: Add a function to handle appending a card to the container element
+// TODO: Style each card differently, based on who the quote is from
+// TODO: Add a button to the card so that we can remove the quote from the array
+// TODO: When the delete button is clicked, remove the quote from the array
+// TODO: Add responsive design
+// TODO: Add Google Fonts
+// TODO: Add the HTML for a Light/Dark Mode button.
+// TODO: Add Light/Dark Mode
+// TODO: Add a Favicon to our site
+
 /*-------------------------------- Constants --------------------------------*/
 
 
